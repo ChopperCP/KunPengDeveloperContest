0e33d4d4d3fec094b0764c4b2c96c0b2e4670849
ChopperCP <568624486@qq.com>
2021-03-29 22:06:26 +0800

    Added some test data

---
+++ Database Design/test_data.sql
@@ -0,0 +1,46 @@
+INSERT INTO `user` values (
+    '063Av0Ga1X8XLA0BMhGa1QCvgB2Av0GA',
+    'employee',
+    '12345678901',
+    'ChopperCP'
+),
+(
+    '123Av0Ga1X8XLA0aahGa1QCvgB2Av0GA',
+    'security',
+    '88888888888',
+    'Zhang 3'
+),
+(
+    '2424242a1X8XLA0BMhGa1QCvgB2Av0GA',
+    'admin',
+    '00000000000',
+    'Admin'
+)
+
+INSERT INTO `port` values (
+    1,
+    123.77777,
+    99.3333,
+    'IN_QR_STR',
+    'OUT_QR_STR',
+    'Gate 1'
+),
+(
+    2,
+    34.78544,
+    89.66666,
+    'IN_QR_STR',
+    'OUT_QR_STR',
+    'Gate 2'
+),
+
+INSERT INTO `record` (wechat_id,port_id,status) values (
+    '063Av0Ga1X8XLA0BMhGa1QCvgB2Av0GA',
+    1,
+    'normal'
+),
+(
+    '123Av0Ga1X8XLA0aahGa1QCvgB2Av0GA',
+    2,
+    'error'
+),
