61079bc015bca7ea517eebb5c3a8e21c41b5a848
rglkt <jnukunting@qq.com>
2021-04-07 10:22:29 +0800

    更新测试数据

--- Database Design/test_data.sql
+++ Database Design/test_data.sql
@@ -24,26 +24,26 @@ INSERT INTO `port` values (
     1,
     123.77777,
     99.3333,
-    'IN_QR_STR',
-    'OUT_QR_STR',
+    'IN_cb4b95c8e5864765b6a3998b6ec02fc0',
+    'OUT_8c9c651940d647c0a3ae47f94cdce68a',
     'Gate 1'
 ),
 (
     2,
     34.78544,
     89.66666,
-    'IN_QR_STR',
-    'OUT_QR_STR',
+    'IN_05361edc1065420786c610e66cf3e465',
+    'OUT_436651b68b1f4448905e75cc8940c966',
     'Gate 2'
 );
 
 INSERT INTO `record` (wechat_id,port_id,status) values (
     '063Av0Ga1X8XLA0BMhGa1QCvgB2Av0GA',
     1,
-    'normal'
+    'IN_NORMAL'
 ),
 (
     '123Av0Ga1X8XLA0aahGa1QCvgB2Av0GA',
     2,
-    'error'
+    'OUT_ERROR'
 );
